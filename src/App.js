5ac3856e1cc003613d63318deaa8f589ae7d5ad1
Mursalin-Dowla <justjim844@gmail.com>
2023-02-06 01:54:55 +0600

    Responsive Header Created

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './Pages/Home/Home/Home';
 import About from './Pages/About/About';
 import Footer from './Pages/Shared/Footer/Footer';
 import Services from './Pages/Home/Services/Services';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Route path='/' element={<Home />}></Route>
       <Route path='/about' element={<About />}></Route>
       <Route path='/services' element={<Services />}></Route>
+      <Route path='/login' element={<Login />}></Route>
     </Routes>
     <Footer />
     </div>
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,29 +1,47 @@
-import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
-import logo from '../../../images/logo.png'
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import logo from "../../../images/logo.png";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <Navbar bg="primary" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark" sticky="top">
+      <Container>
+        <Navbar.Brand as={Link} to="/">
           <img
-              src={logo}
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
-          </Navbar.Brand>
+            src={logo}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt="React Bootstrap logo"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
           </Nav>
-        </Container>
-      </Navbar>
-
-    );
+          <Nav>
+            <Nav.Link as={Link} to="/about">About</Nav.Link>
+            <Nav.Link as={Link} to="/login">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
